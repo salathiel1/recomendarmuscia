fc50a4acba84430e31de3795fd56282061c85145
salathiel <salathiel_souzaviana@hotmail.com>
2017-12-13 23:04:51 -0300

    Id unico

--- src/main/java/redes/controller/FacebookController.java
+++ src/main/java/redes/controller/FacebookController.java
@@ -23,7 +23,8 @@ public class FacebookController {
 
 	private Facebook facebook;
     private ConnectionRepository connectionRepository;
-
+    private static final String PAGE_FIELDS = "id,name";
+    
     @Autowired
     public FacebookController(Facebook facebook, ConnectionRepository connectionRepository) {
         this.facebook = facebook;//new FacebookTemplate("EAACEdEose0cBAMJ6ZBNFeiiJD2GlC3vUyZCyC16wdPqxVQtZBOYjt0Kt9iVTJSPInatE1CXDP1YNZCTOY0aYHINIiFIFT9HnoALruuZBFclGGwqGE2Has7NIIpTW1XyD3zConwZATSRAxCQaxLkVHrNQ6wIpMHZCF1RwFS94QGOgSvg9XcfwIIFlk5DNVLqSZBMZAtVzuNmvZBNwZDZD");
@@ -35,10 +36,10 @@ public class FacebookController {
         if (connectionRepository.findPrimaryConnection(Facebook.class) == null)
             return "redirect:/connect/facebook";
 
-        String [] fields = { "id", "about", "age_range", "birthday", "context", "cover", "currency", "devices", "education", "email", "favorite_athletes", "favorite_teams", "first_name", "gender", "hometown", "inspirational_people", "installed", "install_type", "is_verified", "languages", "last_name", "link", "locale", "location", "meeting_for", "middle_name", "name", "name_format", "political", "quotes", "payment_pricepoints", "relationship_status", "religion", "security_settings", "significant_other", "sports", "test_group", "timezone", "third_party_id", "updated_time", "verified", "video_upload_limits", "viewer_can_send_gift", "website", "work", "music"};
+        String [] fields = {"id", "name"};
         User userProfile = facebook.fetchObject("me", User.class, fields);
         model.addAttribute("perfil", userProfile);
-        PagedList<Page> minhasPaginasMusicas = facebook.likeOperations().getMusic();
+        PagedList<Page> minhasPaginasMusicas = facebook.fetchConnections(userProfile.getId(), "music", Page.class, PAGE_FIELDS);
         model.addAttribute("mpm", minhasPaginasMusicas);
         PagedList<Reference> amigos = facebook.friendOperations().getFriends();
         Page paginaRecomendada = recomendarMusica(minhasPaginasMusicas, amigos);
